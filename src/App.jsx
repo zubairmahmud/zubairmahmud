1a12cd2d96d9574d765016db0bbf805923b7ae61
ZUBAIR MAHMUD <zubayer.2f@gmail.com>
2025-08-04 09:01:06 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
-src/App.jsx
+import React from 'react';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+
+const App = () => {
+  return (
+    <main className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      <Hero />
+      <About />
+      <Skills />
+    </main>
+  );
+};
+
+export default App;
